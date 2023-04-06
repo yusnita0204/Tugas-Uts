84a5f8207b4cfe328210c43c169797fa0565b3b6
yusnita yusnita <itayusnita678@gmail.com>
2023-04-05 22:58:35 -0700

    membuat index

--- index.php
+++ index.php
@@ -12,3 +12,33 @@ $queryHarga = "SELECT * FROM `harga_beras`"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WOI ZAKAT</title>
 </head>
+
+<body>
+    <form method="post">
+        <label for="nama">Nama</label>
+        <input name="nama" type="text"></input><br>
+        <label for="tanggungan">Tanggungan</label>
+        <input name="tanggungan" type="number"></input><br>
+        <select name="hb">
+            <?php
+            foreach ($db->tampil_Data_Banyak($queryHarga) as $key => $value) {
+                ?>
+                <option value="<?php echo $value['id'] ?>">
+                    <?php echo $value['harga_beras'] ?>
+                </option>
+                <?php
+                }
+            ?>
+        </select>
+        <label for="NA">Nama amil</label>
+        <input name="NA" type="text"></input>
+        <button type="submit" name="btn-tambah">Tambah</button>
+    </form>
+    <?php
+    if (isset($_POST['btn-tambah'])) {
+          $nama = htmlspecialchars($_POST['nama']);
+          $tanggungan = htmlspecialchars($_POST['tanggungan']);
+          $hb = htmlspecialchars($_POST['hb']);
+          $idHb = $db->tampil_Data_Satu("SELECT * FROM `harga_beras` WHERE `id`= $hb ");
+          $tb = intval($tanggungan) * intval($idHb['harga_beras']) * 4.3;
+          $NA = htmlspecialchars($_POST['NA']);
